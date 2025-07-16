4751e858607d28ea8cb17ade5b1c1f71ec6212cb
agapebondservant <agapebondservant@yahoo.com>
2025-07-16 03:20:35 +0000

    updates

--- gen_keys.py
+++ gen_keys.py
@@ -21,7 +21,8 @@ def sign_message(challenge, filename="secret_key.txt"):
 
     # TODO recover your account information for your private key and sign the given challenge
     # Use the code from the signatures assignment to sign the given challenge
-    account = w3.eth.account.from_key(key[0])
+    private_key = key[0]
+    account = w3.eth.account.from_key(private_key)
     signed_message = w3.eth.account.sign_message(message, private_key=private_key)
 
 
